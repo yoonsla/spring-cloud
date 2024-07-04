e63544f5200a3c268c75b331f4bff00d0511d412
yoonsla <sangyoon5_4@naver.com>
2024-07-04 16:14:51 +0900

    spring cloud config setting

---
+++ config/config-server/src/main/java/com/sy/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.sy.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
