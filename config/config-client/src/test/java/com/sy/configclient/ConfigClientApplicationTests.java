42905e3bca360dd6497b3397204f5c7943520971
yoonsla <sangyoon5_4@naver.com>
2024-07-04 16:17:15 +0900

    spring cloud config setting #2

---
+++ config/config-client/src/test/java/com/sy/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sy.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
